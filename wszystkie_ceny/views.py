c32313b0f77762eb10fd38e086fb710c5b08d283
piter1316 <piter_1316@wp.pl>
2018-10-24 11:41:19 +0200

    new sql queries fast search

--- wszystkie_ceny/views.py
+++ wszystkie_ceny/views.py
@@ -10,18 +10,25 @@ from porownanie_cen.models import Produkt
 
 
 def wszystkie_ceny_view(request):
-    products_list = Produkt.objects.all()
+    products_list = Produkt.objects.raw("select * from produkty limit 20")
     query = request.GET.get('q')
     paginator = Paginator(products_list, 15)
     page = request.GET.get('page')
     products = paginator.get_page(page)
+    result=None
+
+
+    # if query:
+    #     products = products_list.filter(kodtowaru__contains=query).order_by('cenakoncowa_eur')
+
+    if query:
+        result = Produkt.objects.raw("SELECT * from produkty where kodtowaru = '{}'".format(query))
 
     context = {
-        'products': products,
+        'result': result,
+        'products': products_list,
+        'query': query
     }
-    if query:
-        products = products_list.filter(kodtowaru__contains=query).order_by('cenakoncowa_eur')
-
     return render(request, 'wszystkie_ceny/produkty.html', context)
 
 
